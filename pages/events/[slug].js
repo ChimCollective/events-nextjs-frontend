79148bf34464fbcbdc4810b050fa54cafb0476c3
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-05-07 10:00:37 -0400

    Next Link & Router

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,8 @@
-import { useRouter } from "next/router";
-
 export default function EventPage() {
-  const router = useRouter();
 
   return (
     <div>
       <h1>My Event</h1>
-      <h3>{router.query.slug}</h3>
-      <button onClick={() => router.push("/")}>Click</button>
     </div>
   );
 }
