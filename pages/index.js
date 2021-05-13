f7ac686a545134d094b63582a817ad749c49b9d2
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2021-05-13 16:10:18 -0400

    Fixed Minor bugs and 403 Privet Route

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,12 @@ import { API_URL } from '@/config/index'
 import Link from 'next/link'
 
 export default function HomePage({ events }) {
+
   return (
     <Layout>
       <h1>Upcoming Events</h1>
       {events.length === 0 && <h3>No events to show</h3>}
-      {events.map(evt => (
+      {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
 
